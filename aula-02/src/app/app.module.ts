ff5fe52a32be407e90447a3d26caacc0d85dcc69
Alvaro Jose Baranoski <alvarojosebaranoski01@gmail.com>
2023-06-15 02:32:41 +0000

    aula 2

---
+++ aula-02/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AlunoFormComponent } from './aluno-form/aluno-form.component';
+import { CursoFormComponent } from './curso-form/curso-form.component';
+import { FormsModule } from '@angular/forms';
+import { ProdutoFormComponent } from './produto-form/produto-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AlunoFormComponent,
+    CursoFormComponent,
+    ProdutoFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ aula-02/src/app/curso-form/curso-form.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-curso-form',
+  templateUrl: './curso-form.component.html',
+  styleUrls: ['./curso-form.component.css']
+})
+export class CursoFormComponent {
+
+  constructor() {
+
+  }
+
+  onSubmit() {
+    console.log('olá mundo');
+  }
+
+}
